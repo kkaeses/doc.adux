26cfdfa5f187a815672ed0b6bd82df4c67d277e4
kkaeses <kkaeses@adux.com>
2017-05-11 07:26:04 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wp_users.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_users.sql
@@ -32,7 +32,7 @@ CREATE TABLE `wp_users` (
 
 LOCK TABLES `wp_users` WRITE;
 /*!40000 ALTER TABLE `wp_users` DISABLE KEYS */;
-INSERT INTO `wp_users` VALUES (1,'kkaeses','$P$Bop0jr/Hq7KXBBpaZN72ui8ZPu0yrx.','kkaeses','kkaeses@adux.com','','2017-02-24 09:01:45','',0,'kkaeses'),(2,'gdecant','$P$BBWyTzmo.yH84jcQahES5UyRFcxguV.','gdecant','gdecant@adux.com','','2017-04-13 07:51:35','',0,'Geoffrey Decant'),(3,'avandendriessche','$P$BTOah28Sn4HU6iBANBdy/bmO1j2zey/','avandendriessche','avandendriessche@adux.com','','2017-04-21 07:17:07','',0,'Adrien Van den Driessche');
+INSERT INTO `wp_users` VALUES (1,'kkaeses','$P$Bop0jr/Hq7KXBBpaZN72ui8ZPu0yrx.','kkaeses','kkaeses@adux.com','','2017-02-24 09:01:45','',0,'kkaeses'),(2,'gdecant','$P$BspPfKmWgvrd0dbusQ8kv/nwMBXcjC1','gdecant','gdecant@adux.com','','2017-04-13 07:51:35','',0,'Geoffrey Decant'),(3,'avandendriessche','$P$BTOah28Sn4HU6iBANBdy/bmO1j2zey/','avandendriessche','avandendriessche@adux.com','','2017-04-21 07:17:07','',0,'Adrien Van den Driessche');
 /*!40000 ALTER TABLE `wp_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
